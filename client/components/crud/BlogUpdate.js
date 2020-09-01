ad9b244fa4773c444de7515f60196a8e7d04018d
Hoang-Minh <quataoxanh83@gmail.com>
2020-09-01 12:15:35 -0700

    Populated content in rich text area in update blog form

--- client/components/crud/BlogUpdate.js
+++ client/components/crud/BlogUpdate.js
@@ -6,17 +6,97 @@ import { withRouter } from "next/router";
 import { getCookie, isAuth } from "../../actions/auth";
 import { getCategories } from "../../actions/category";
 import { getTags } from "../../actions/tag";
-import { create } from "../../actions/blog";
+import { singleBlog, update } from "../../actions/blog";
 import { quillModules, quillFormats } from "../../helpers/quill";
 import "../../node_modules/react-quill/dist/quill.snow.css";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 const BlogUpdate = ({ router }) => {
+  const [body, setBody] = useState({});
+  const [values, setValues] = useState({
+    error: "",
+    success: "",
+    formData: "",
+    title: "",
+    body: "",
+  });
+  const { slug } = router.query;
+  const { error, success, formData, title } = values;
+
+  useEffect(() => {
+    setValues({ ...values, formData: new FormData() });
+    initBlog();
+  }, [slug]);
+
+  const initBlog = () => {
+    if (slug) {
+      singleBlog(slug).then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          setValues({ ...values, title: data.title });
+          setBody(data.body);
+        }
+      });
+    }
+  };
+
+  const handleChange = (name) => (event) => {
+    console.log(event.target.value);
+    const value = name === "photo" ? event.target.files[0] : event.target.value;
+    formData.set(name, value);
+    setValues({
+      ...values,
+      [name]: value,
+      formData,
+      error: "",
+    });
+  };
+
+  const handleBody = (event) => {
+    setBody(event);
+    formData.set("body", event);
+  };
+
+  const editBlog = () => {
+    console.log("update blog");
+  };
+
+  const updateBlogForm = () => {
+    return (
+      <form onSubmit={editBlog}>
+        <div className="form-group">
+          <label className="text-muted">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={handleChange("title")}
+          ></input>
+        </div>
+        <div className="form-group">
+          <ReactQuill
+            modules={quillModules}
+            formats={quillFormats}
+            value={body}
+            placeholder="Blog something...."
+            onChange={handleBody}
+          ></ReactQuill>
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary">
+            Publish
+          </button>
+        </div>
+      </form>
+    );
+  };
+
   return (
     <div className="container-fluid pb-5">
       <div className="row">
         <div className="col-md-8">
-          <p>Create bog form</p>
+          {updateBlogForm()}
           <div className="pt-3">show success and error msg</div>
         </div>
         <div className="col-md-4">
