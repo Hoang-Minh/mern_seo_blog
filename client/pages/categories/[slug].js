f7f3f05c3c0444fae547a7e2b61e90bb249e3e90
Hoang-Minh <quataoxanh83@gmail.com>
2020-09-02 10:02:04 -0700

    Added seo for categories and tags page

--- client/pages/categories/[slug].js
+++ client/pages/categories/[slug].js
@@ -8,9 +8,44 @@ import { getCategory } from "../../actions/category";
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from "../../config";
 import Card from "../../components/blog/Card";
 
-const Category = ({ category, blogs }) => {
+const Category = ({ category, blogs, query }) => {
+  const head = () => (
+    <Head>
+      <title>
+        {category.name} | {APP_NAME}
+      </title>
+      <meta
+        name="description"
+        content={`Best programming tutorials on ${category.name}`}
+      ></meta>
+      <link rel="canonical" href={`${DOMAIN}/categories/${query.slug}`}></link>
+      <meta
+        property="og:title"
+        conent={`${category.name} | ${APP_NAME}`}
+      ></meta>
+      <meta
+        property="og:description"
+        content={`Best programming tutorials on ${category.name}`}
+      ></meta>
+      <meta property="og:type" content="website"></meta>
+      <meta
+        property="og:url"
+        content={`${DOMAIN}/categories/${query.slug}`}
+      ></meta>
+      <meta property="og:site_name" content={`${APP_NAME}`}></meta>
+      <meta property="og:image" content="/static/images/seoblog.jfif"></meta>
+      <meta
+        property="og:image:secure_url"
+        content="/static/images/seoblog.jfif"
+      ></meta>
+      <meta property="og:image:type" content="image/jfif"></meta>
+      <meta property="fb:app_id" content={`${FB_APP_ID}`}></meta>
+    </Head>
+  );
+
   return (
     <React.Fragment>
+      {head()}
       <Layout>
         <main>
           <div className="container-fluid text-center">
@@ -34,7 +69,7 @@ Category.getInitialProps = ({ query }) => {
     if (data.error) {
       console.log(data.error);
     } else {
-      return { category: data.category, blogs: data.blogs };
+      return { category: data.category, blogs: data.blogs, query };
     }
   });
 };
