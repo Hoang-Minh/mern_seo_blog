5b48910c739251048e5afa334d2d58ef05e4d40a
Hoang-Minh <quataoxanh83@gmail.com>
2020-09-10 11:14:55 -0700

    Added Contact Form

--- client/components/form/ContactForm.js
+++ client/components/form/ContactForm.js
@@ -3,9 +3,112 @@ import Link from "next/link";
 import { emailContactForm } from "../../actions/form";
 
 const ContactForm = () => {
+  const [values, setValues] = useState({
+    message: "",
+    name: "",
+    email: "",
+    sent: false,
+    buttonText: "Send Message",
+    success: false,
+    error: false,
+  });
+
+  const { message, name, email, sent, buttonText, success, error } = values;
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: "Sending..." });
+    emailContactForm({ name, email, message }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({
+          ...values,
+          setnt: true,
+          name: "",
+          email: "",
+          message: "",
+          buttonText: "Sent",
+          success: true,
+        });
+      }
+    });
+  };
+
+  const handleChange = (name) => (event) => {
+    setValues({
+      ...values,
+      [name]: event.target.value,
+      error: false,
+      success: false,
+      buttonText: "Send Message",
+    });
+  };
+
+  const showSuccessMessage = () =>
+    success && (
+      <div className="alert alert-info">Thank you for contacting us</div>
+    );
+
+  const showErrorMessage = () =>
+    error && (
+      <div
+        className="alert alert-danger"
+        style={{ display: error ? "" : "none" }}
+      >
+        {error}
+      </div>
+    );
+
+  const contactForm = () => {
+    return (
+      <form onSubmit={clickSubmit}>
+        <div className="form-group">
+          <label className="lead">Message</label>
+          <textarea
+            onChange={handleChange("message")}
+            type="text"
+            className="form-control"
+            value={message}
+            required
+            rows="10"
+          ></textarea>
+        </div>
+
+        <div className="form-group">
+          <label className="lead">Name</label>
+          <input
+            type="text"
+            onChange={handleChange("name")}
+            className="form-control"
+            value={name}
+            required
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <label className="lead">Email</label>
+          <input
+            type="email"
+            onChange={handleChange("email")}
+            className="form-control"
+            value={email}
+            required
+          ></input>
+        </div>
+
+        <div>
+          <button className="btn btn-primary">{buttonText}</button>
+        </div>
+      </form>
+    );
+  };
+
   return (
     <React.Fragment>
-      <p>Show Contact Form</p>
+      {showSuccessMessage()}
+      {showErrorMessage()}
+      {contactForm()}
     </React.Fragment>
   );
 };
