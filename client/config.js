3812fd6a41b9dab892a400190e8bad523fd5fbeb
Hoang-Minh <quataoxanh83@gmail.com>
2020-09-15 14:30:56 -0700

    Updated env variables for front end

--- client/config.js
+++ client/config.js
@@ -2,7 +2,7 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://seoblog.com"
+  ? "https://secure-hamlet-36410.herokuapp.com"
   : "http://localhost:5000";
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
--- client/next.config.js
+++ client/next.config.js
@@ -3,9 +3,9 @@ const withCSS = require("@zeit/next-css");
 module.exports = withCSS({
   publicRuntimeConfig: {
     APP_NAME: "SEOBLOG",
-    API_DEVELOPMENT: "http://localhost:5000/api",
-    PRODUCTION: false,
-    DOMAIN_DEVELOPMENT: "http://localhost:3000",
+    API_DEVELOPMENT: "https://secure-hamlet-36410.herokuapp.com:5000/api",
+    PRODUCTION: true,
+    DOMAIN_DEVELOPMENT: "https://secure-hamlet-36410.herokuapp.com:3000",
     DOMAN_PRODUCTION: "",
     FB_APP_ID: "326626271786396",
     DISQUS_SHORTNAME: "blog-j4kipqrcet",
